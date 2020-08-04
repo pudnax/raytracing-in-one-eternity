fc4e24c5d557466d6dc4dd0cfaf18701b74acefe
Alex Komissarov <k.a.komissar@gmail.com>
2020-08-04 18:55:08 +0500

    CPP: Gamma correction

--- cpp/src/color.hpp
+++ cpp/src/color.hpp
@@ -6,15 +6,15 @@
 
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color, int samples_per_pixels) {
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
   auto b = pixel_color.z();
 
-  auto scale = 1.0 / samples_per_pixels;
-  r *= scale;
-  g *= scale;
-  b *= scale;
+  auto scale = 1.0 / samples_per_pixel;
+  r = sqrt(scale * r);
+  g = sqrt(scale * g);
+  b = sqrt(scale * b);
 
   out << static_cast<int>(255.999 * clamp(r, 0.0, 0.999)) << ' '
       << static_cast<int>(255.999 * clamp(g, 0.0, 0.999)) << ' '
