291e36ae535a5eadaaf1eca6e8d2cb54814b013e
Alex Komissarov <k.a.komissar@gmail.com>
2020-08-09 14:39:14 +0500

    CPP: Marble texture

--- cpp/src/perlin.hpp
+++ cpp/src/perlin.hpp
@@ -19,9 +19,9 @@ inline double trilinear_interp(double c[2][2][2], double u, double v,
 class perlin {
 public:
   perlin() {
-    randfloat = new double[point_count];
+    ranvec = new vec3[point_count];
     for (int i = 0; i < point_count; ++i) {
-      randfloat[i] = random_double();
+      ranvec[i] = unit_vector(vec3::random(-1, 1));
     }
 
     perm_x = perlin_generate_perm();
@@ -29,39 +29,49 @@ public:
     perm_z = perlin_generate_perm();
   }
 
-  ~perlin() {
-    delete[] randfloat;
-    delete[] perm_x;
-    delete[] perm_y;
-    delete[] perm_z;
+  double turb(const point3 &p, int depth = 7) const {
+    auto accum = 0.0;
+    auto temp_p = p;
+    auto weight = 1.0;
+
+    for (int i = 0; i < depth; ++i) {
+      accum += weight * noise(temp_p);
+      weight *= 0.5;
+      temp_p *= 2;
+    }
+
+    return fabs(accum);
   }
 
   double noise(const point3 &p) const {
     auto u = p.x() - floor(p.x());
     auto v = p.y() - floor(p.y());
     auto w = p.z() - floor(p.z());
-	            u = u*u*(3-2*u);
-            v = v*v*(3-2*v);
-            w = w*w*(3-2*w);
-
     int i = floor(p.x());
     int j = floor(p.y());
     int k = floor(p.z());
-    double c[2][2][2];
+    vec3 c[2][2][2];
 
     for (int di = 0; di < 2; di++)
       for (int dj = 0; dj < 2; dj++)
         for (int dk = 0; dk < 2; dk++)
           c[di][dj][dk] =
-              randfloat[perm_x[(i + di) & 255] ^ perm_y[(j + dj) & 255] ^
-                        perm_z[(k + dk) & 255]];
+              ranvec[perm_x[((i + di) & 255)] ^ perm_y[((j + dj) & 255)] ^
+                     perm_z[((k + dk) & 255)]];
 
-    return trilinear_interp(c, u, v, w);
+    return perlin_interp(c, u, v, w);
+  }
+
+  ~perlin() {
+    delete[] ranvec;
+    delete[] perm_x;
+    delete[] perm_y;
+    delete[] perm_z;
   }
 
 private:
   static const int point_count = 256;
-  double *randfloat;
+  vec3* ranvec;
   int *perm_x;
   int *perm_y;
   int *perm_z;
@@ -85,6 +95,25 @@ private:
       p[i] = p[target];
       p[target] = tmp;
     }
+  }
+
+  inline double perlin_interp(vec3 c[2][2][2], double u, double v,
+                              double w) const {
+    auto uu = u * u * (3 - 2 * u);
+    auto vv = v * v * (3 - 2 * v);
+    auto ww = w * w * (3 - 2 * w);
+    auto accum = 0.0;
+
+    for (int i = 0; i < 2; i++)
+      for (int j = 0; j < 2; j++)
+        for (int k = 0; k < 2; k++) {
+          vec3 weight_v(u - i, v - j, w - k);
+          accum += (i * uu + (1 - i) * (1 - uu)) *
+                   (j * vv + (1 - j) * (1 - vv)) *
+                   (k * ww + (1 - k) * (1 - ww)) * dot(c[i][j][k], weight_v);
+        }
+
+    return accum;
   }
 };
 
--- cpp/src/texture.hpp
+++ cpp/src/texture.hpp
@@ -55,7 +55,7 @@ public:
   noise_texture(double sc) : scale(sc) {}
 
   virtual color value(double u, double v, const point3 &p) const override {
-    return color(1, 1, 1) * noise.noise(scale*p);
+    return color(1, 1, 1) * 0.5 * (1 + sin(scale * p.z() + 10 * noise.turb(p)));
   }
 
 public:
