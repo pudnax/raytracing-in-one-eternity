dbfd35cafed4f9fe25d79f9e5c7ef64bea9577f1
Alex Komissarov <k.a.komissar@gmail.com>
2020-08-11 13:15:37 +0500

    Rust: New perlin.rs

--- rust/src/texture.rs
+++ rust/src/texture.rs
@@ -1,1 +1,24 @@
 use std::sync::Arc;
+
+use crate::{perlin, vec3::Vec3};
+
+pub type Texture = Arc<dyn Fn(Vec3) -> Vec3 + Send + Sync>;
+
+pub fn constant(color: Vec3) -> Texture {
+    Arc::new(move |_| color)
+}
+
+pub fn checker(t0: Texture, t1: Texture) -> Texture {
+    Arc::new(move |p| {
+        let s = (10. * p).map(f32::sin).reduce(std::ops::Mul::mul);
+        if s < 0. {
+            t1(p)
+        } else {
+            t0(p)
+        }
+    })
+}
+
+pub fn perlin(scale: f32) -> Texture {
+    Arc::new(move |p| Vec3::from(perlin::turb(scale * p, 7)))
+}
