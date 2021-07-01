1980963516101c198f0064a077d1a30943097a8a
Alex Komissarov <k.a.komissar@gmail.com>
2021-07-01 18:51:47 +0500

    Can't stop golfing :c

--- rust/src/perlin.rs
+++ rust/src/perlin.rs
@@ -2,25 +2,19 @@ use rand::prelude::*;
 
 use crate::vec3::Vec3;
 
-fn generate_perm(rng: &mut impl Rng) -> Vec<u8> {
-    let size = 256;
-    let mut p = Vec::with_capacity(size);
-    for i in 0..size {
-        p.push(i as u8);
-    }
+const SIZE: u8 = 255;
 
-    for i in (1..size).rev() {
+fn generate_perm(rng: &mut impl Rng) -> Vec<u8> {
+    let mut p: Vec<_> = (0..=SIZE).collect();
+
+    for i in (1..=SIZE as usize).rev() {
         p.swap(i, rng.gen_range(0..i));
     }
     p
 }
 
 fn generate_vecs(rng: &mut impl Rng) -> Vec<Vec3> {
-    let mut f = Vec::with_capacity(256);
-    for _ in 0..256 {
-        f.push(Vec3::in_unit_sphere(rng));
-    }
-    f
+    (0..=SIZE).map(|_| Vec3::in_unit_sphere(rng)).collect()
 }
 
 lazy_static::lazy_static! {
@@ -30,44 +24,40 @@ lazy_static::lazy_static! {
     pub static ref PERM_Z: Vec<u8> = generate_perm(&mut thread_rng());
 }
 
-#[rustfmt::skip]
-fn trilinear_interp(corners: &[[[Vec3; 2]; 2]; 2], uvw: Vec3) -> f64 {
+fn trilinear_interp<'a>(corner_iter: impl Iterator<Item = &'a Vec3>, uvw: Vec3) -> f64 {
     let uvw3 = uvw * uvw * (Vec3::from(3.) - 2. * uvw);
     let uvw3_inv = Vec3::from(1.) - uvw3;
-    corners.iter().flatten().flatten().enumerate()
-        .fold(0., |accum, (idx, corner)| {
-            let (i, j, k) = (idx & 1, (idx & 2) / 2, (idx & 4) / 4);
-            let ijk = Vec3(i as f64, j as f64, k as f64);
-            let weight = corner.dot(uvw - ijk);
-            let ijk_inv = Vec3::from(1.) - ijk;
-            accum + (ijk + uvw3 + ijk_inv * uvw3_inv).reduce(std::ops::Mul::mul) * weight
-        })
+    corner_iter.enumerate().fold(0., |accum, (idx, corner)| {
+        let (i, j, k) = (idx & 1, (idx & 2) / 2, (idx & 4) / 4);
+        let ijk = Vec3(i as f64, j as f64, k as f64);
+        let weight = corner.dot(uvw - ijk);
+        let ijk_inv = Vec3::from(1.) - ijk;
+        accum + (ijk + uvw3 + ijk_inv * uvw3_inv).reduce(std::ops::Mul::mul) * weight
+    })
+}
+
+#[allow(dead_code)]
+fn trilinear_interp_array(corners: &[[[Vec3; 2]; 2]; 2], uvw: Vec3) -> f64 {
+    trilinear_interp(corners.iter().flatten().flatten(), uvw)
 }
 
 pub fn noise(p: Vec3) -> f64 {
     let ijk = p.map(f64::floor);
     let uvw = p - ijk;
     let mut corners = [[[Vec3::default(); 2]; 2]; 2];
-    for di in 0..2 {
-        for dj in 0..2 {
-            for dk in 0..2 {
-                let ix = PERM_X[((ijk.0 as i32 + di as i32) & 255) as usize];
-                let iy = PERM_Y[((ijk.1 as i32 + dj as i32) & 255) as usize];
-                let iz = PERM_Z[((ijk.2 as i32 + dk as i32) & 255) as usize];
-                corners[di][dj][dk] = VECS[(ix ^ iy ^ iz) as usize];
-            }
-        }
+    for (idx, corner) in corners.iter_mut().flatten().flatten().enumerate() {
+        let (di, dj, dk) = (idx & 1, (idx & 2) / 2, (idx & 4) / 4);
+        let ix = PERM_X[((ijk.0 as i32 + di as i32) & 255) as usize];
+        let iy = PERM_Y[((ijk.1 as i32 + dj as i32) & 255) as usize];
+        let iz = PERM_Z[((ijk.2 as i32 + dk as i32) & 255) as usize];
+        *corner = VECS[(ix ^ iy ^ iz) as usize];
     }
-    trilinear_interp(&corners, uvw)
+    trilinear_interp_array(&corners, uvw)
 }
 
-pub fn turb(mut p: Vec3, depth: usize) -> f64 {
-    let mut accum = 0.;
-    let mut weight = 1.;
-    for _ in 0..depth {
-        accum += weight * noise(p);
-        weight *= 0.5;
-        p = 2. * p;
-    }
-    accum.abs()
+pub fn turb(p: Vec3, depth: usize) -> f64 {
+    (1..depth + 1)
+        .map(|i| (1. / i as f64, p * i as f64))
+        .fold(0., |accum, (weight, p)| accum + weight * noise(p))
+        .abs()
 }
